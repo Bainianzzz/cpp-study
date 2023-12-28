15d5d385fe626902a34ce67ffa86a9f01a9d9fab
Bainianzzz <3036349123@qq.com>
2023-12-28 17:41:13 +0800

    课上练习的迭代和递归解

--- func/prac1.cpp
+++ func/prac1.cpp
@@ -1,12 +1,22 @@
 #include<iostream>
 using namespace std;
 
+double f(double k);
+
 int main() {
 	double k = 1,ans=0;
 	for (k=1;k <= 10;k++) {
 		ans += 1 / (k * (k + 1));
 	}
 	cout << ans<<endl;
+	cout << f(10) << endl;
 	system("pause");
 	return 0;
+}
+
+double f(double k) {
+	if (k == 1)
+		return 0.5;
+	else
+		return f(k - 1) + 1 / (k * (k + 1));
 }
