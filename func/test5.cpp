35da9c700527bb7982661756ec02ba92b9b70989
Bainianzzz <3036349123@qq.com>
2023-12-28 17:10:02 +0800

    模拟卷第五题

--- func/test5.cpp
+++ func/test5.cpp
@@ -2,15 +2,15 @@
 using namespace std;
 
 int main(){
-	double ans = 0,an=1,i=1;
+	double sn = 0,an=1,i=1;
 
 	while(an >= 1e-6){
 		an =  (1.0 / (3.0 * i - 2.0));
-		ans += pow(-1, i - 1) *an;
+		sn += pow(-1, i - 1) *an;
 		i++;
 	}
 
-	cout <<ans<<endl;
+	cout <<sn<<endl;
 
 	system("pause");
 	return 0;
