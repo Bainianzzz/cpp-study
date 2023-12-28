a5b28bfcdd993512a42fd3568bbc8b5099c68d85
Bainianzzz <3036349123@qq.com>
2023-12-28 22:27:19 +0800

    课上练习的迭代和递归解

--- func/prac1.cpp
+++ func/prac1.cpp
@@ -5,11 +5,14 @@ double f(double k);
 
 int main() {
 	double k = 1,ans=0;
+
 	for (k=1;k <= 10;k++) {
 		ans += 1 / (k * (k + 1));
 	}
+
 	cout << ans<<endl;
 	cout << f(10) << endl;
+	
 	system("pause");
 	return 0;
 }
