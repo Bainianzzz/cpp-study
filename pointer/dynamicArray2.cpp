ef2f6543f74673be849c2f46680f1c8c2d35198b
Bainianzzz <3036349123@qq.com>
2024-01-02 14:39:37 +0800

    动态数组（new）

---
+++ pointer/dynamicArray2.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+
+void print(int *a, int size);
+
+int main(){
+	//使用new和delete申请和释放内存
+	int size;
+	int *dArray;
+
+	cin >> size;
+	dArray = new int[size];
+
+	memset(dArray, 0, size*sizeof(int));
+	print(dArray, size);
+
+	delete[] dArray;
+
+	system("pause");
+	return 0;
+}
+
+void print(int *a, int size){
+	for (int i = 0; i < size; i++){
+		cout << *(a + i) << ", ";
+	}
+	cout << endl;
+}
